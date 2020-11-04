72a83d55a35c77466a0fa1b55f37511bc0a5cbb8
Henry Lee <henryleeworld@gmail.com>
2020-11-04 23:32:45 +0800

    feat:升級至 laravel 8

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserAgentParserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/device/info', 'UserAgentParserController@getDeviceInfo');
+Route::get('/device/info', [UserAgentParserController::class, 'getDeviceInfo']);
